67b5f8d0ebe2ef07e842e9ac4bcb5d926e235870
Dwight Marie <dwightmarie@gmail.com>
2019-10-16 01:54:46 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,7 @@ def bonus
 
   #code your solution here:
 
- pic_tragedy[:monta egue][:hero][:status] = "dead"
+ epic_tragedy ontague][:hero][:status] = "dead"
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
