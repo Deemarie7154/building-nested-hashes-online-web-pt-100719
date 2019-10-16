17c8b18fb12abee0ee1a842fb33b2072e3c51a52
Dwight Marie <dwightmarie@gmail.com>
2019-10-16 01:19:06 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,20 +1,20 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+      :montague => {
       #your key/value pairs here!
-      :patriarch => {},
+        :patriarch => {},
         :matriarch => {},
         :hero => {},
         :hero_friends => []
       }, 
-    :capulet => {
+      :capulet => {
       #your key/value pairs here!
-      :patriarch => {},
+       :patriarch => {},
        :matriarch => {},
        :heroine => {},
        :heroine_friends => []
-     }
-      }
+        }
+  }
 
   
 end
