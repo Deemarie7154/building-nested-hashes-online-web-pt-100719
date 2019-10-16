78f6b3fb452d9a9b96816c24bc241fa7045adc24
Dwight Marie <dwightmarie@gmail.com>
2019-10-16 01:09:48 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,18 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy =   {
+     :montague => 
+        :patriarch => {name: "Lord Montague", age: "53"},
+        :matriarch => {name: "Lady Montague", age: "53"},
+        :hero => {name: "Romeo",},
+        :hero_friends => []
+     
+   },
+     :capulet => {
+       :patriarch => {},
+       :matriarch => {},
+       :heroine => {},
+       :heroine_friends => []
+     }
+   }
+   
 end
